d14022f766c6ecdb2617cbc082be397a2cc129e5
thalitafeitosa <124682848+thalitafeitosa@users.noreply.github.com>
2023-05-06 13:52:38 -0300

    primeiro commt pokedex

---
+++ eventos.js
@@ -0,0 +1,22 @@
+
+fetch("https://pokeapi.co/api/v2/pokemon")
+.then( res => res.json())
+.then(lista =>  {
+    lista.results.forEach(cada => {
+        pokemons.innerHTML += `<option value="${cada.url}">${cada.name}</option>`;
+        
+    });
+})
+
+function buscarPokemon() {
+    fetch(pokemons.value)
+    .then(res => res.json())
+    .then(pokemon => {
+        document.querySelector(".pokemon").innerHTML = `<img src="${pokemon.sprites.front_default}">`
+        document.querySelector(".tipo").innerHTML = "";
+        pokemon.types.forEacth(cada => {
+            document.querySelector(".tipo").innerHTML+= `<span>${cada.type.name} ,</span>`;
+        })
+            
+    })
+}
